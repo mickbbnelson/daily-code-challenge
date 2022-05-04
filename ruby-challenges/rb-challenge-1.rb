e130f4241f1cc067683aaf29710ab8f5888ca782
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-04 14:24:39 -0600

    Complete Ruby Challenge 1

---
+++ ruby-challenges/rb-challenge-1.rb
@@ -0,0 +1,8 @@
+# Find the amount of given numbers in array that are greater than 5k
+
+testArray = [1,9,8,2,3,7,6,4,5]
+
+greater_than_5 = testArray.filter{|num| num > 5 }.count
+
+print greater_than_5
+
