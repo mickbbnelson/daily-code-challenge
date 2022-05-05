2f91b98c28662454b6673263077b8627a91dae5a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-05 16:50:51 -0600

    Complete js-challenge-2

---
+++ javaScript-challenges/js-challenge-2.js
@@ -0,0 +1,9 @@
+// Create a function that converts kilometers into miles
+
+function lengthConverter(kilos){
+    return kilos * .62137229
+}
+
+const testCase = lengthConverter(5)
+
+console.log(testCase)
