df904d6cdf20c77ca448e12396d28386f97226f9
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-15 22:03:23 -0600

    Update code challenge 6

--- javaScript-challenges/js-challenge-6.js
+++ javaScript-challenges/js-challenge-6.js
@@ -3,26 +3,38 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
-testArray1 = [1,2,3,4,5]
-testArray2 = [2,0,6,7]
-testArray3 = [1,2,5,6]
+testArray1 = [1,2,2,3,5]
+testArray2 = [0,1,1,7]
+testArray3 = [1,2,5,7]
 
 testTarget1 = 5
 testTarget2 = 2
-testTarget3 = 7
+testTarget3 = 6
 
 function sumFilter(newArray, number, sum){
     for (let i = 0; i < newArray.length; i++)
         if (newArray[i] + number === sum) {
-            return i
+            return [newArray[i], i]
         }
 }
 
 function twoSum(array, target) {
     for (let i = 0; i < array.length; i++) {
         const newArray = array.slice(i)
-        const newNumber = sumFilter(newArray, array[i], target)
-            return [i, newNumber]
+        // const newNumber = sumFilter(newArray, array[i], target)
+        const testNumber = array[i]
+        let secondNumber = ""
+        let secondIndex = ""
+        for (let i = 0; i < newArray.length; i++)
+            if (newArray[i] + testNumber === target) {
+                secondNumber = newArray[i]
+                secondIndex = i + 1
+            }
+            if (i === 0 && array[i] + secondNumber === target) {
+                return [i, secondIndex - 1]
+            } else if (array[i] + secondNumber === target) {
+                return [i, secondIndex]  
+            }
     }
 }
 
